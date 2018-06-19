cf67e8abb89eecce44496d2872098f34a0fd93a3
hellooying <15642330153@163.com>
2018-06-19 23:57:55 +0800

    移除元素

--- removeElement.java
+++ removeElement.java
@@ -1,18 +1,17 @@
 public class removeElement {
     public static void main(String[] args) throws Exception {
-        int[] a = { 1,1,1,1,1,1};
-        v=1;
+        int[] a = { 4,1,2,4};
+        int v=1;
         System.out.println(new removeElement().answer(a,v));
     }
 
     public int answer(int[] nums, int val) {
         int q=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==val){
-                q++;
-                nums=remove(nums,i);
-            }
-           // nums[i]=nums[i+q];
+        int i=0;
+        while(i+q<nums.length){
+            if(nums[i]==val)  q++;
+            else i++;
+            if(i+q<nums.length)  nums[i]=nums[i+q];
         }
         return nums.length-q;
     }
