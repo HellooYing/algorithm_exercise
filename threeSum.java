477db5561110f113a9d5a12661874d1bcffb8021
hellooying <15642330153@163.com>
2018-06-23 02:07:12 +0800

    都是一万多行数据，凭什么说我的不一样，我要看看哪不一样

--- threeSum.java
+++ threeSum.java
@@ -2,22 +2,19 @@ import java.util.*;
 
 public class threeSum {
     public static void main(String[] args) throws Exception {
-        int[] x = { -1, 0, 1, 2, -1, -4};
-        ArrayList<ArrayList<Integer>> a=new threeSum().answer(x);
+        int[] x = {-2,0,1,1,2};
+        List<List<Integer>> a=new threeSum().answer(x);
         System.out.println(a);
     }
-    public ArrayList<ArrayList<Integer>> answer(int[] nums) {
-        ArrayList<ArrayList<Integer>> result= new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> list= new ArrayList<Integer>();
+    public List<List<Integer>> answer(int[] nums) {
+        List<List<Integer>> result= new ArrayList<List<Integer>>();
         Arrays.sort(nums);
         int i=1;
         int j=nums.length-1;
         int k=0;
         int m;
-        System.out.println(Arrays.toString(nums));
         while(k<nums.length-1){
-            System.out.println("i:"+i+",j:"+j+",k:"+k);
-            if(j==0||i==nums.length-1){
+            if(j==0||i==nums.length-1||j==i){
                 k++;
                 i=k+1;
                 j=nums.length-1;
@@ -27,14 +24,16 @@ public class threeSum {
             if(m>0) j--;
             else if(m<0) i++;
             else if(m==0){
-                list.clear();
+                ArrayList<Integer> list= new ArrayList<Integer>();
                 list.add(nums[k]);
                 list.add(nums[i]);
                 list.add(nums[j]);
-                result.add(list);
-                k++;
-                i=k+1;
-                j=nums.length-1;
+                int judge=0;
+                for(int d=0;d<result.size();d++){
+                    if(result.get(d).get(0)==list.get(0)&&result.get(d).get(1)==list.get(1)&&result.get(d).get(2)==list.get(2)) judge=1;
+                }
+                if(judge==0) result.add(list);
+                i++;
             }
         }
         return result;
