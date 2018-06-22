383f2b506cf5b805d285a9d0e27e219bab2c2e2b
hellooying <15642330153@163.com>
2018-06-23 01:23:44 +0800

    中等题三数之和，看了很久java的list啥的，现在要了解一下java的垃圾回收

--- threeSum.java
+++ threeSum.java
@@ -2,15 +2,42 @@ import java.util.*;
 
 public class threeSum {
     public static void main(String[] args) throws Exception {
-        int[] x = { 1,3,5,4,2 };
+        int[] x = { -1, 0, 1, 2, -1, -4};
         ArrayList<ArrayList<Integer>> a=new threeSum().answer(x);
         System.out.println(a);
     }
     public ArrayList<ArrayList<Integer>> answer(int[] nums) {
-        ArrayList<ArrayList<Integer>> list= new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> list1= new ArrayList<Integer>(Arrays.asList(nums));
-        list.add(list1);
-        return list;
+        ArrayList<ArrayList<Integer>> result= new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list= new ArrayList<Integer>();
+        Arrays.sort(nums);
+        int i=1;
+        int j=nums.length-1;
+        int k=0;
+        int m;
+        System.out.println(Arrays.toString(nums));
+        while(k<nums.length-1){
+            System.out.println("i:"+i+",j:"+j+",k:"+k);
+            if(j==0||i==nums.length-1){
+                k++;
+                i=k+1;
+                j=nums.length-1;
+                continue;
+            }
+            m=nums[k]+nums[i]+nums[j];
+            if(m>0) j--;
+            else if(m<0) i++;
+            else if(m==0){
+                list.clear();
+                list.add(nums[k]);
+                list.add(nums[i]);
+                list.add(nums[j]);
+                result.add(list);
+                k++;
+                i=k+1;
+                j=nums.length-1;
+            }
+        }
+        return result;
     }
 }
 
