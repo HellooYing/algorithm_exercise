a7509ad6515661b76229140c4a28fc5de8bbe7a0
hellooying <15642330153@163.com>
2018-09-21 23:41:54 +0800

    别人在打cf，我没干活，伤心

---
+++ removeNthFromEnd.java
@@ -0,0 +1,14 @@
+public class removeNthFromEnd {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+}
