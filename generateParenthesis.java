208e076bf1e16ef39c1b9e7c86a724f255a5c8f9
hellooying <15642330153@163.com>
2018-10-01 23:57:02 +0800

    我不听我不听这就是今日份的虽然我还没写完

---
+++ generateParenthesis.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+public class generateParenthesis {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        List<String> l=new generateParenthesis().answer(a);
+        for(int aa:l)
+            System.out.println(aa);
+    }
+
+    public List<String> answer(int n) {
+
+    }
+}
