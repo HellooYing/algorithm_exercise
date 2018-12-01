33c990ad8e340e2bf6c4ec00c5d08faa94282677
hellooying <15642330153@163.com>
2018-12-01 23:27:41 +0800

    剑指offer 从上往下打印二叉树

--- PrintFromTopToBottom.java
+++ PrintFromTopToBottom.java
@@ -19,8 +19,6 @@ public class PrintFromTopToBottom {
         }
         TreeNode tmp=find(root);
         tmp.val=x;
-        frontPrint(root);
-        System.out.println();
     }
 
     public void frontPrint(TreeNode node){
@@ -28,7 +26,26 @@ public class PrintFromTopToBottom {
         if(node.left!=null) frontPrint(node.left);
         if(node.right!=null) frontPrint(node.right);
     }
-    
+
+    public int height(TreeNode node){
+        int n=1;
+        int l=0,r=0;
+        if(node.left!=null) l=height(node.left,n);
+        if(node.right!=null)  r=height(node.right,n);
+        if(l>=r&&l>n) n=l;
+        else if(r>l&&r>n) n=r;
+        return n;
+    }
+
+    public int height(TreeNode node, int n){
+        n++;
+        int l=0,r=0;
+        if(node.left!=null) l=height(node.left,n);
+        if(node.right!=null)  r=height(node.right,n);
+        if(l>=r&&l>n) n=l;
+        else if(r>l&&r>n) n=r;
+        return n;
+    }
     public TreeNode find(TreeNode node){//find输入node，输出以node为根节点的完全二叉树的下一个节点（也就是第一个空节点）
         //逻辑是，
         //如果左节点为null，就返回左节点，否则如果右节点为null，返回右节点，否则：
@@ -78,10 +95,36 @@ public class PrintFromTopToBottom {
         tree.add(6);
         tree.add(7);
         tree.add(8);
+        tree.add(9);
+        tree.add(10);
+        tree.add(11);
+        tree.add(12);
+        tree.add(13);
+        tree.add(14);
+        tree.add(15);
+        tree.add(16);
+        tree.add(17);
+        tree.add(18);
+        System.out.print(tree.answer(root));
     }
-    
+    public void dg(TreeNode node,ArrayList<ArrayList<Integer>> r,int h){
+        r.get(h).add(node.val);
+        h++;
+        if(node.left!=null) dg(node.left,r,h);
+        if(node.right!=null) dg(node.right,r,h);
+    }
     public ArrayList<Integer> answer(TreeNode root){
-        ArrayList<Integer> r=new ArrayList<Integer>();
-        return r;
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        if(root==null) return result;
+        ArrayList<ArrayList<Integer>> r=new ArrayList<ArrayList<Integer>>();
+        //分层打印二叉树，先获取深度，然后创建二维数组
+        //第1层，h=1-1=0;r.get(h).add(node.val);
+        //如果有左节点就递归dg(root.left,r,h);    r.get(h).add(node.val);如果有左节点就递归dg(node.left,r,h);  
+        int height;
+        height=height(root);
+        for(int i=0;i<height;i++) r.add(new ArrayList<Integer>());
+        dg(root,r,0);
+        for(int i=0;i<height;i++) result.addAll(r.get(i));
+        return result;
     }
 }
