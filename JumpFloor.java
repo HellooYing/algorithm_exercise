e4f9084ecf9cc43300e4d7dcbaa04a19e964697c
hellooying <15642330153@163.com>
2018-12-04 11:22:12 +0800

    剑指offer：跳台阶  对于牛客或者leetcode运行结果与本地不符，可能的原因是上次运算留下的数据可能影响这次，因此要手动清一下0

---
+++ JumpFloor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class JumpFloor {
+    public static void main(String[] args) throws Exception {
+        System.out.println(new JumpFloor().answer(2));
+    }
+    static int n=0;
+    void dg(int target){
+        if(target==1||target==2) n++;
+        if(target>1) dg(target-1);
+        if(target>2) dg(target-2);
+    }
+    public int answer(int target){
+        n=0;
+        dg(target);
+        return n;
+    }
+
+}
